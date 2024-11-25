0c8f2691c990c2fe8eb2e4ba8f08c2b26bc7c8d6
Abdurahmon <ak2527@live.mdx.ac.uk>
2024-11-25 16:11:31 +0000

    change to the cors inclusion

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'https://abdurahmon-kamariddin.github.io',
+        methods: ['GET', 'POST', 'PUT']
+    }
+));
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -16,21 +21,21 @@ const ordersCollection = db.collection("orders");
 
 app.get("/clubs", async (request, response) => {
     console.log("GET /clubs");
-    const products = clubCollection.find({}).toArray( function(err, results) {
+    const products = clubCollection.find({}).toArray(function (err, results) {
         if (err) {
             return nextTick(err);
             console.log(err);
         }
         response.send(results);
         console.log(results);
-        });
     });
+});
 
 
 app.post("/saveOrder", async (request, response) => {
     console.log("POST /saveOrder");
     const order = request.body;
-    ordersCollection.insertOne(order, function(err, result) {
+    ordersCollection.insertOne(order, function (err, result) {
         if (err) {
             return nextTick(err);
             console.log(err);
@@ -43,14 +48,14 @@ app.post("/saveOrder", async (request, response) => {
 app.put("/updateAvailability", async (request, response) => {
     console.log("PUT /updateAvailability");
     const cart = request.body.cart;
-    for (const item of cart){
+    for (const item of cart) {
         try {
-        var clubID = item;
-        const result = await clubCollection.updateOne(
-            { id: clubID },
-            { $inc: { availableSeats: -1 } }
-        );
-        console.log("Updated club with ID " + clubID + " availability. Result:" + result);
+            var clubID = item;
+            const result = await clubCollection.updateOne(
+                { id: clubID },
+                { $inc: { availableSeats: -1 } }
+            );
+            console.log("Updated club with ID " + clubID + " availability. Result:" + result);
         } catch (error) {
             console.log("Error updating availability for club " + clubID + ". Err: " + error);
         }
@@ -59,6 +64,8 @@ app.put("/updateAvailability", async (request, response) => {
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function() { {
-    console.log("Server is running on port " + port);
-}});
+app.listen(port, function () {
+    {
+        console.log("Server is running on port " + port);
+    }
+});
